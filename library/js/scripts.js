e040512974696da2ce0dfc3c53f666dfa765e74f
Leslie <lhochsz@gmail.com>
2016-12-22 13:53:12 -0500

    Copy updates, new screenshot, play button now black

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -48,10 +48,10 @@ if ((is_chrome)&&(is_opera)) {is_chrome=false;}
 
 //Check to see if it's a mobile device and/or if it's playing in Safari browser
 if (isMobile.any()) {
-	$("#slx-play").attr("href", "http://www.youtube.com/watch?v=m02aHT61scA");
+	$("#slx-play").attr("href", "http://www.youtube.com/watch?v=Q5rImLpatBI");
 } else if (is_safari) {
 	$("#slx-play").attr("href", "#");
-	$("#explain").html("360&deg; video does not work in the Safari.  Please try watching it in Chrome, <br>Firefox,&nbsp;Internet Explorer or Opera.");
+	$("#explain").html("Oh! It looks like you're using Safari! <br>360 and Safari don't get along. <br>It's like Bette Davis and Joan Crawford. Try a friendlier browser like Chrome, <br>Firefox, Opera or even Internet Explorer <br>(I know, we were surprised too!).");
 } else {
 	$('#slx-play').on('click', function(event){
 		event.preventDefault();
@@ -61,13 +61,13 @@ if (isMobile.any()) {
 }
 
 if (isMobile.iOS()) {
-	$('#explain').html("You'll need to YouTube app to view the 360&deg; video.  If you dont have it, <a href='https://itunes.apple.com/us/app/youtube-watch-share-videos/id544007664?mt=8' target='_blank'>click here</a> to download.");
+	$('#explain').html("You must be a person about town, <br>checking this on your phone on the go. <br>You'll need the YouTube app to see this awesome 360&deg; video. If you don't already have it, we made it easy for ya'. <br>Just <a href='https://itunes.apple.com/us/app/youtube-watch-share-videos/id544007664?mt=8' target='_blank'>click here</a>.");
 } else if (isMobile.Android()) {
-	$('#explain').html("You'll need to YouTube app to view the 360&deg; video.  If you dont have it, <a href='https://play.google.com/store/apps/details?id=com.google.android.youtube&hl=en' target='_blank'>click here</a> to download.");
+	$('#explain').html("You must be a person about town, <br>checking this on your phone on the go. <br>You'll need the YouTube app to see this awesome 360&deg; video. If you don't already have it, we made it easy for ya'. <br>Just <a href='https://play.google.com/store/apps/details?id=com.google.android.youtube&hl=en' target='_blank'>click here</a>.");
 } else if (isMobile.Windows()) {
- 	$('#explain').html("You'll need to download some apps to view the 360&deg; video on a Microsoft device.  If you dont have it, download the <a href='https://www.microsoft.com/en-us/store/p/tubecast-for-youtube/9wzdncrdx3fs' target='_blank'>Tubecast</a> app and the <a href='https://www.microsoft.com/store/apps/9nblggh23xvv' target='_blank'>Video 360</a>, or try it on a desktop.");
+ 	$('#explain').html("Exclamation! You're a freethinker with your Windows Phone! <br>But you'll need to download some apps to view this awesome 360 video on a Microsoft device. <br>Use the <a href='https://www.microsoft.com/en-us/store/p/tubecast-for-youtube/9wzdncrdx3fs' target='_blank'>Tubecast</a> and <a href='https://www.microsoft.com/store/apps/9nblggh23xvv' target='_blank'>Video 360</a> apps or your computer.");
 } else if (isMobile.BlackBerry() || isMobile.Opera()) {
-  	$('#explain').text("The 360&deg; video may not work correctly on your device.  Please try it on a desktop.");
+  	$('#explain').text("Gasp! You’re either on a Blackberry or an Opera Phone! <br>Sadly, 360 and your phone don't get along. <br>Like Bette Davis and Joan Crawford. <br>Instead, try this video on your computer ASAP.");
 }
 
 
